2c27ef8f35143e860ba7036beb41b8ad823632fc
nhaug <mail@n-haug.de>
2021-02-09 21:48:27 +0100

    Füge DB Langtest hinzu

---
+++ lang_test/sqliteConnect.js
@@ -0,0 +1,36 @@
+const sqlite3 = require('sqlite3').verbose();
+
+try {
+  // open in-Memory DB
+  let db = new sqlite3.Database(':memory:',(err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Connected to the in-memory SQLite Database");
+  });
+
+  // close DB
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Close the Database Connection");
+  });
+
+  // Create new file-based DB
+  let dba = new sqlite3.Database('./db/test.db', (err) => {
+    if (err) {
+      console.error(err.message+err.stack);
+    }
+    console.log('Connected to the chinook database.');
+  });
+
+  dba.close((err) => {
+    if (err) {
+        console.error(err.message + errs.stack);
+    }
+  })
+
+} catch (e) {
+  console.error("Fehler"+e + e.stack);
+}
---
+++ lang_test/sqliteQuery.js
@@ -0,0 +1,52 @@
+const sqlite3 = require('sqlite3');
+
+// Create new file-based DB
+let db = new sqlite3.Database('./db/chinook.db', (err) => {
+  if (err) {
+    console.error(err.message+err.stack);
+  }
+  console.log('Connected to the chinook database.');
+});
+
+let sql = 'SELECT DISTINCT PlaylistId id, Name name FROM playlists ORDER BY id';
+
+// Alle Reihen db.all()
+// db.all(sql, params, (err, rows) => {
+db.all(sql, [], (err, rows) => {
+  // Verarbeite rows hier
+  rows.forEach((row) => {
+    console.log("%d | %s",row.id, row.name);
+  })
+});
+
+// Einzelne Reihe: db.get()
+listId = 1;
+sql = `SELECT PlaylistId id, Name name
+       FROM playlists
+      WHERE PlaylistId = ?`;
+db.get(sql, [listId], (err, row) => {
+  return row
+    ? console.log("%d | %s",row.id, row.name)
+    : console.log("No playlist found with the id %s",listId);
+})
+// Besser Reihen nacheinander: db.each()
+sql = `SELECT FirstName firstName,
+                  LastName lastName,
+                  Email email
+            FROM customers
+            WHERE Country = ?
+            ORDER BY FirstName`;
+
+db.each(sql, ['USA'], (err, row) => {
+  if (err) {
+    throw err;
+  }
+  // console.log(`${row.firstName} ${row.lastName} - ${row.email}`);
+  console.log('%s %s - %s', row.firstName, row.lastName, row.email);
+});
+
+db.close((err) => {
+  if (err) {
+      console.error(err.message + errs.stack);
+  }
+})
